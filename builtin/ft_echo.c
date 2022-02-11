94ef2a0b8bd9817baf4eebd5b2a3695da8597f34
Young il Cho <young-ch@c1r9s5.42seoul.kr>
2022-02-11 18:16:04 +0900

    echo

--- builtin/ft_echo.c
+++ builtin/ft_echo.c
@@ -1,26 +1,52 @@
 #include "../include/minishell.h"
 
+static int	check_nflag(char *str, int *flag)
+{
+	if (*(str++) != '-')
+		return (FALSE);
+	if (*str != 'n')
+		return (FALSE);
+	while (*str != '\0')
+	{
+		if (*str != 'n')
+			return (FALSE);
+		str++;
+	}
+	*flag = 1;
+	return (TRUE);
+}
+
+static int	get_flag(char **cmd, int *flag)
+{
+	int		i;
+
+	i = 1;
+	if (cmd[i] == NULL)
+		return (i);
+	while (cmd[i] != NULL)
+	{
+		if (check_nflag(cmd[i], flag) == FALSE)
+			return (i);
+		i++;
+	}
+	return (i);
+}
+
 int	ft_echo(char **cmd)
 {
 	int		flag;
 	int		i;
 
-	flag = -1;
-	if (cmd[1] != NULL)
+	flag = 0;
+	i = get_flag(cmd, &flag);
+	while (cmd[i] != NULL)
 	{
-		flag = ft_strcmp(cmd[1], "-n");
-		i = 1;
-		if (flag == 0)
-			i = 2;
-		while (cmd[i] != NULL)
-		{
-			printf("%s", cmd[i]);
-			if (cmd[i + 1] != NULL)
-				printf(" ");
-			i++;
-		}
+		printf("%s", cmd[i]);
+		if (cmd[i + 1] != NULL)
+			printf(" ");
+		i++;
 	}
-	if (flag != 0)
+	if (flag == 0)
 		printf("\n");
 	return (0);
 }
