942ea62fce8e209ffd8076979c3101ae2e5d152c
aseungbo <seungbo1112@gmail.com>
2022-02-21 17:24:05 +0900

    Modified WASD, Arrow Key

--- main.c
+++ main.c
@@ -20,7 +20,19 @@ int	arg_check(int argc, char **argv)
 int	deal_key(int key_code, t_cub *game)
 {
 	(void)game;
-	if (key_code == KEY_ESC)
+	if (key_code == KEY_W)
+		printf("KEY W\n");
+	else if (key_code == KEY_A)
+		printf("KEY A\n");
+	else if (key_code == KEY_S)
+		printf("KEY S\n");
+	else if (key_code == KEY_D)
+		printf("KEY D\n");
+	else if (key_code == KEY_LEFT_ARROW)
+		printf("KEY LEFT\n");
+	else if (key_code == KEY_RIGHT_ARROW)
+		printf("KEY RIGHT\n");
+	else if (key_code == KEY_ESC)
 	{
 		printf("KEY_PRESS");
 		exit(0);
