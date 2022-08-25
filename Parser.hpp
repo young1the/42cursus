70d37a0a2a9169a6828708063500231929bddcaf
Young il Cho <young-ch@c2r3s1.42seoul.kr>
2022-08-25 14:56:34 +0900

    ADD parser (todo:implement checkVector exception)

--- Parser.hpp
+++ Parser.hpp
@@ -3,6 +3,8 @@
 #include <iostream>
 #include <sstream>
 #include <utility>
+#include <exception>
+#include <cctype>
 
 class Parser
 {
@@ -13,6 +15,82 @@ private:
     
     Vector _v;
 
+    void initVector(const std::string & str)
+    {
+        unsigned long prev_index = 0;
+        unsigned long deli_index = str.find(' ');
+        while (deli_index != std::string::npos)
+        {
+            std::string substring = str.substr(prev_index, deli_index - prev_index);
+            if (substring != "")
+                _v.push_back(std::make_pair(substring, NONE));
+            prev_index = deli_index + 1;
+            if (str[prev_index] == ':')
+            {
+                _v.push_back(std::make_pair(str.substr(prev_index), NONE));
+                return ;
+            }
+            deli_index = str.find(' ', prev_index);
+        }
+        std::string substring = str.substr(prev_index, deli_index - prev_index);
+        if (substring != "")
+            _v.push_back(std::make_pair(substring, NONE));
+    }
+
+    void setType()
+    {
+        int i = 0;
+        if (_v.empty())
+            throw std::logic_error("Wrong Format!");
+        if (_v[i].first[0] == ':')
+        {
+            _v[i++].second = PREFIX;
+        }
+        _v[i++].second = COMMAND;
+        for (; i < _v.size(); ++i)
+        {
+            if (_v[i].first[0] == ':')
+            {
+                _v[i].second = TRAILING;
+                return ;
+            }
+            _v[i].second = PARAMS;
+        }
+    }
+
+    void checkVector()
+    {
+        for (int i = 0; i < _v.size(); ++i)
+        {
+            switch (_v[i].second)
+            {
+                case PREFIX:
+                    _v[i].first.erase(_v[i].first.begin());
+                case COMMAND:
+                    if(!checkStr(_v[i].first, isalpha))
+                        throw std::logic_error("Wrong Format!!");
+                    break ;
+                case PARAMS:
+                    break ;
+                case TRAILING:
+                    _v[i].first.erase(_v[i].first.begin());
+                    break ;
+            }
+        }
+    }
+
+    bool checkStr(const std::string & str, int (*f)(int))
+    {
+        for (int i = 0; i < str.size(); ++i)
+        {
+            if (!f(str[i]))
+                return false;
+        }
+        return true;
+    }
+
+public:
+    
     /** <message>  ::= [':' <prefix> <SPACE> ] <command> <params> <crlf>
         <prefix>   ::= <servername> | <nick> [ '!' <user> ] [ '@' <host> ]
         <command>  ::= <letter> { <letter> } | <number> <number> <number>
@@ -29,48 +107,32 @@ private:
         PREFIX = 0,
         COMMAND = 1,
         PARAMS = 2,
+        TRAILING = 3,
         END,
     };
 
-    void initVector(const std::string & str)
-    {
-        unsigned long prev_index = 0;
-        unsigned long deli_index = str.find(' ');
-        while (deli_index != std::string::npos)
-        {
-            std::string substring = str.substr(prev_index, deli_index - prev_index);
-            _v.push_back(std::make_pair(substring, NONE));
-            prev_index = deli_index + 1;
-            if (str[prev_index] == ':')
-            {
-                _v.push_back(std::make_pair(str.substr(prev_index), NONE));
-                return ;
-            }
-            deli_index = str.find(' ', prev_index);
-        }
-        _v.push_back(std::make_pair(str.substr(prev_index, deli_index - prev_index), NONE));
-    }
-
-    void setType()
-    {
-        int i = 0;
-        if (_v[i].first[0] == ':')
-        {
-            _v[i++].second = PREFIX;
-        }
-        _v[i++].second = COMMAND;
-        for (; i < _v.size(); ++i)
-        {
-            _v[i].second = PARAMS;
-        }
-    }
-
-public:
-    
     Parser(const std::string & str)
     {
         initVector(str);
         setType();
+        checkVector();
+    }
+
+    Parser(const std::string & str, const char & delimeter)
+    {
+        unsigned long prev_index = 0;
+        unsigned long deli_index = str.find(delimeter);
+        while (deli_index != std::string::npos)
+        {
+            std::string substring = str.substr(prev_index, deli_index - prev_index);
+            if (substring != "")
+                _v.push_back(std::make_pair(substring, NONE));
+            prev_index = deli_index + 1;
+            deli_index = str.find(delimeter, prev_index);
+        }
+        std::string substring = str.substr(prev_index, deli_index - prev_index);
+        if (substring != "")
+            _v.push_back(std::make_pair(substring, NONE));
     }
 
     ~Parser();
--- test.cpp
+++ test.cpp
@@ -4,8 +4,17 @@ int main()
 {
     // std::string str1 = "Hello World Young";
     // std::string str2 = ":Hello World Young";
-    std::string str1 = ":Hello World Young :hello uni";
+    std::string str1 = ":Hello Wor1ld Young :hello uni";
+    std::string str2 = "   CAP,LS   ";
 
-    Parser p1(str1);
-    p1.printVector();
+    // std::string str1 = "::        ";
+    try
+    {
+        Parser p1(str1);
+        p1.printVector();
+    }
+    catch (const std::exception & e)
+    {
+        std::cout << e.what() << std::endl;
+    }
 }
